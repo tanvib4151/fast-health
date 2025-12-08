a193bff7a0d33ca8e2a5adaa22c95c15851ad686
Tanvi Bharadwaj <tanvibharadwaj99@gmail.com>
2025-12-07 18:49:15 -0800

    Added email and password

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+class Person(BaseModel):
+    FirstName: str
+    LastName: str
+    Email: str
+    Password: str
+
+@app.post("/")
+def register (person: Person):
+    return {"FirstName": person.FirstName, "LastName": person.LastName, "Email": person.Email, "Password": person.Password}
