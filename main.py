616935d3330f204e2ff177d16666a37176c55d07
Tanvi Bharadwaj <tanvibharadwaj99@gmail.com>
2025-12-27 18:34:53 -0800

    test

--- main.py
+++ main.py
@@ -16,3 +16,58 @@ class Person(BaseModel):
 @app.post("/")
 def register (person: Person):
     return {"FirstName": person.FirstName, "LastName": person.LastName, "Email": person.Email, "Password": person.Password}
+
+@app.get("/tracking-points/{user_id}")
+def get_tracking_points(user_id: int):
+    return{
+  "tracking_points": [
+    {
+      "id": "tp_1",
+      "type": "symptom",
+      "user_selected": True
+    },
+    {
+      "id": "tp_2",
+      "type": "factor",
+      "user_selected": False
+    },
+    {
+      "id": "tp_3",
+      "type": "treatment",
+      "user_selected": False
+    },
+    {
+      "id": "tp_4",
+      "type": "mood",
+      "user_selected": False
+    }
+  ]
+}
+
+@app.get("/tracking-points/{tracking_points_id}/subpoints")
+
+def get_subpoints(tracking_points_id: int):
+    return{
+  "subpoints": [
+    {
+      "id": "s1",
+      "name": "Headache",
+      "user_selected": True,
+    },
+    {
+      "id": "s2",
+      "name": "Nausea",
+      "user_selected": False,
+    },
+    {
+      "id": "s3",
+      "name": "Pain",
+      "user_selected": False,
+    },
+    {
+      "id": "s4",
+      "name": "Fatigue",
+      "user_selected": False,
+    }
+  ]
+}
