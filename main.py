34dc6f2c60c9309b03eb56cb2984b8ccbeb23fda
Tanvi Bharadwaj <tanvibharadwaj99@gmail.com>
2026-01-11 18:22:27 -0800

    removed cors

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,  # IMPORTANT
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def root():
     return {"message": "hi"}
