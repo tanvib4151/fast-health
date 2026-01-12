041f30cc6a3aa12050cf4029f9b224489a344841
Tanvi Bharadwaj <tanvibharadwaj99@gmail.com>
2026-01-11 18:45:20 -0800

    changed allow credentials to true

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=False,  # IMPORTANT
+    allow_credentials=True,  # IMPORTANT
     allow_methods=["*"],
     allow_headers=["*"],
 )
